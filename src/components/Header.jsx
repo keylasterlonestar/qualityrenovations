e5ff210b52f39c65db1abad2e3aeb35f1ee3b14f
keylasterlonestar <joeandkelly7411@gmail.com>
2026-04-10 15:34:46 -0500

    Replace placeholder icon with real logo in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -22,16 +22,7 @@ export default function Header() {
       <div className="container">
         <div className="header-inner">
           <div className="logo">
-            <div className="logo-icon">
-              <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/>
-                <polyline points="9 22 9 12 15 12 15 22" fill="none" stroke="white" strokeWidth="1.5"/>
-              </svg>
-            </div>
-            <div className="logo-text">
-              Quality Renovators
-              <span>Licensed &amp; Insured</span>
-            </div>
+            <img src="/logo.png" alt="Quality Renovators Logo" className="logo-img" />
           </div>
 
           <nav className="nav">
